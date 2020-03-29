3de986af257a8b32aef73b2bfebde0f4549333f1
g4rcez <allan.f.garcez@gmail.com>
2020-03-29 02:03:50 -0300

    all other files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { ActionTest } from "./actions/test";
+import fillState from "./lib/fill-state";
+import useRedux from "./lib/use-connect";
+import useInjectReducer from "./lib/use-inject-reducer";
+import {
+  MainReducer,
+  mainReducerInitialReducer
+} from "./reducers/main-reducer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const actions = { ActionTest: ActionTest.dispatch };
+
+const App = () => {
+  useInjectReducer("MainReducer", MainReducer);
+  const [state, dispatch] = useRedux(state => {
+    const MainReducer = fillState(state.MainReducer, mainReducerInitialReducer);
+    return {
+      n: MainReducer.number
+    };
+  }, actions);
+
+  useEffect(() => {
+    setTimeout(() => dispatch.ActionTest(9), 3000);
+  }, [dispatch]);
+
+  return <span>{JSON.stringify(state)}</span>;
+};
 
 export default App;
